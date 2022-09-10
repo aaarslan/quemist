2090428aff934838b2602b8e0ae2cdb19cd64fc9
Abdallah Arslan <aarslan@Abdallahs-MacBook-Pro.local>
2022-09-10 12:47:30 -0400

    fixed code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import Element from './Element';
 import {Segment} from 'semantic-ui-react'
 
-
 function App() {
     return (
         <div className='ui container fluid'>
-            <div></div>
-            <Segment className="ui container fluid">
-                <Element/>
+            <Segment>
+            <div>
+              <h1>Quemist</h1>
+              <p>Coming soon... A webapp to empower the Chemists in your life</p>
+            </div>
+                <Segment className="ui container fluid">
+                    <Element/>
+                </Segment>
             </Segment>
 
         </div>
